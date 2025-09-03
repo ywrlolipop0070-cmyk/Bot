0f40b8d081057474bc94cebe95303311b6220665
sahim09 <sahimuthali01@gmail.com>
2025-09-04 05:48:16 +0600

    Rushi.js

--- Script/commands/Rushi.js
+++ Script/commands/Rushi.js
@@ -2,7 +2,7 @@ module.exports.config = {
  name: "rushia",
  version: "1.0.0",
  hasPermssion: 0,
- credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+ credits: "Farhan 😺👋",
  description: "Random Rushia",
  commandCategory: "random-img",
  usages: "rushia",
