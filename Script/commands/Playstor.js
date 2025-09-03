80d155b7dfff250c45d3db0e0e441546268e53f7
sahim09 <sahimuthali01@gmail.com>
2025-09-04 05:44:55 +0600

    Playstor.js

--- Script/commands/Playstor.js
+++ Script/commands/Playstor.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "playstore",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+  credits: "Farhan 😺👋",
   description: "",
   commandCategory: "User",
   usages: "",
