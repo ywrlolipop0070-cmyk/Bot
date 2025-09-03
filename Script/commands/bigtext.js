235d5a7af336b4dcf151db4cfe2bd8e882296140
sahim09 <sahimuthali01@gmail.com>
2025-09-04 05:54:12 +0600

    bigtext.js

--- Script/commands/bigtext.js
+++ Script/commands/bigtext.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "bigtext",
     version: "1.0.0",
     hasPermssion: 0,
-    credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+    credits: "Farhan 😺👋",
     description: "big text",
     commandCategory: "game",
     usages: "bigtext <text>",
