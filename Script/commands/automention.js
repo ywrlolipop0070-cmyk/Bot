85946e97e920580975d3377965b1aca3ee898c7f
sahim09 <sahimuthali01@gmail.com>
2025-09-04 06:24:02 +0600

    automention.js

--- Script/commands/automention.js
+++ Script/commands/automention.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "automention",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "Farhan 😺👋",
 	description: "automent [mentioned]",
 	commandCategory: "other",
 	cooldowns: 5
