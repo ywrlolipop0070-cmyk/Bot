f94b2713c3b3b9d79a4cdcdf298368b73b3f6a01
sahim09 <sahimuthali01@gmail.com>
2025-09-04 06:48:01 +0600

    autosetname.js

--- Script/events/autosetname.js
+++ Script/events/autosetname.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "autosetname",
 	eventType: ["log:subscribe"],
 	version: "1.0.3",
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "Farhan 😺👋",
 	description: "Automatically set new member nicknames"
 };
 
