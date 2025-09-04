8b1af980d74e6f58880ca2187ee111d519870fea
sahim09 <sahimuthali01@gmail.com>
2025-09-04 06:25:33 +0600

    chor.js

--- Script/commands/chor.js
+++ Script/commands/chor.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "chor",
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "Joshua Sy",
+	credits: "Farhan 😺👋",
 	description: "scooby doo template memes",
 	commandCategory: "Picture",
 	usages: "...",
