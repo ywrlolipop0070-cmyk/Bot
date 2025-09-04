c4aa52017d10052af662b2e9e89f22efe2dfe565
sahim09 <sahimuthali01@gmail.com>
2025-09-04 06:17:31 +0600

    autoreact.js

--- Script/commands/autoreact.js
+++ Script/commands/autoreact.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "autoreact",
   version: "1.1.1",
   hasPermission: 0,
-  credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+  credits: "Farhan 😺👋",
   description: "Bot React",
   commandCategory: "No Prefix",
   usages: '[]',
