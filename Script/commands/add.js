9379d19aaf4876e74661276da8312d8a31860903
sahim09 <sahimuthali01@gmail.com>
2025-09-04 06:15:12 +0600

    add.js

--- Script/commands/add.js
+++ Script/commands/add.js
@@ -4,7 +4,7 @@ module.exports.config = {
  name: "add",
  version: "1.0.0",
  hasPermission: 0,
- credits: "Shaon",
+ credits: "Farhan 👋👽",
  description: "Send a random sad video",
  commandCategory: "media",
  usages: "",
