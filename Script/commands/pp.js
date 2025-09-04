f5725bd4d3c3bd1a20b65cd4c348e816dc8f5386
sahim09 <sahimuthali01@gmail.com>
2025-09-04 06:43:32 +0600

    pp.js

--- Script/commands/pp.js
+++ Script/commands/pp.js
@@ -2,7 +2,7 @@ module.exports.config = {
  name: "pp",
  version: "1.0.0",
  hasPermssion: 0,
- credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+ credits: "Farhan 😺👋",
  description: "Lấy ID người dùng.",
  commandCategory: "Công cụ",
  cooldowns: 0
